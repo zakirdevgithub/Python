245c0f300b64c73e268b554cf133c32704a32d47
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:29:28 +0600

    Add files via upload

---
+++ 06.Calculate.py
@@ -0,0 +1,13 @@
+print(500+100)
+print(500-100)
+print(500*100)
+print(500/100)
+print("-------------------------")
+print(2**3)
+print(2**3**2)
+print("--------------------------")
+print(8/2) #float Division
+print(8//2) #int Division
+print("--------------------------")
+print(7/2) #float Division
+print(7//2) #int Division
