f067b79c3b794c22dc36206fef0d767c6fa45587
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:30:33 +0600

    Add files via upload

---
+++ 08.user_input.py
@@ -0,0 +1,9 @@
+# name=input("Enter Your name: ")
+# age=input("Enter Your age: ")
+# print(name,age)
+#
+# name,age=input("Enter your name and age:(separate by space) ").split()
+# print(name,age)
+
+name,age=input("Enter your name and age:(separate by comma) ").split(",")
+print(name,age)
