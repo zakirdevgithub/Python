68b0c05d3552898b50b8473dd70ee2b0ef53b8e3
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:54:16 +0600

    Add files via upload

---
+++ 36. filter.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 19 03:16:36 2019
+
+@author: progr
+"""
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+
+even=list(filter(lambda a:a%2==0 ,numbers))
+
+print(even)
