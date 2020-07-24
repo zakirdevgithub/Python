ef93a9d236cecbd228e5d75fe1461cb7920ed006
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:34:18 +0600

    Add files via upload

---
+++ 13.strip_center.py
@@ -0,0 +1,13 @@
+name="           za        kir                "
+dots="--------------------------"
+print(name+dots)
+print(name.lstrip()+dots)
+print(name.rstrip()+dots)
+print(name.strip()+dots)
+print(name.replace(" ","")+dots)
+
+country="Bangladesh"
+print(country.center(len(country)+8,"*"))
+
+roommate="Rakib"
+print(roommate.center(len(roommate)+30, "&"))
