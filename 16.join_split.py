3945c4688f8f3a237ba1a12129e415bbcef367fe
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:36:30 +0600

    Add files via upload

---
+++ 16.join_split.py
@@ -0,0 +1,9 @@
+#split
+#convert string to List
+name="Bangladesh is a beautiful country"
+print(name.split(" "))
+
+team=["Rakib","Shuvo","Sabbir","Zakir"]
+print(" ".join(team))
+print("*".join(team))
+print("#".join(team))
