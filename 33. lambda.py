f3154ca089f8304b5a44a62e2f68ca361618659e
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:53:01 +0600

    Add files via upload

---
+++ 33. lambda.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 19 00:26:51 2019
+
+@author: progr
+"""
+
+var=lambda a,b: a+b
+print(var(10,20))
+
+multiply=lambda a,b:a*b
+print(multiply(10,20))
+
+is_true= lambda s: len(s)>5
+print(is_true("Bangladesh"))
+print(is_true("abc"))
