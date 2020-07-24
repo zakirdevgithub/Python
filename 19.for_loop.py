419c46dc037a5b727d49f70a5a6c2ceb672e66be
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:38:07 +0600

    Add files via upload

---
+++ 19.for_loop.py
@@ -0,0 +1,21 @@
+for i in range(10):
+    print(i)
+
+for i in range(10,50):
+    print(i)
+
+for i in range(2,20,3):
+    print(i)
+
+sentence="""Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum."""
+for i in range(len(sentence)):
+     print("My name is zakir")
+
+for i in range(10):
+    print(i, end=" ") #For one line
+    print(i, end=",")
