3f4789bc78b7576ca05278070502e12d8c40e7c8
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:53:47 +0600

    Add files via upload

---
+++ 35. map.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 19 02:56:47 2019
+
+@author: progr
+"""
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+
+even=list(map(lambda a:a**2, numbers))
+print(even)
+
+names=["Zakir","Zahid","Jewel","setu","joy"]
+
+length= list(map(len,names))
+print(length)
