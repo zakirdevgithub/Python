7b9ba93026149812f9fd1306b7d66bdc7aca0a8b
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:10:44 +0600

    Add files via upload

---
+++ 02.escafe_sequence.py
@@ -0,0 +1,7 @@
+# print("hi my "name" is zakir") error
+print("hi my \"name\" is zakir")
+#I want to print one backslash before last inverted comma
+# print("How are you guys\")
+print("How are you guys? \\")
+#I want to print two backslash before last inverted comma
+print("How old are you? \\\\")
