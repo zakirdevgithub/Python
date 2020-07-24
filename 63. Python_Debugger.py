f25e114e3b6ca10f9cfffb49930c9612d1c10734
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 20:05:42 +0600

    Add files via upload

---
+++ 63. Python_Debugger.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 04:29:37 2019
+
+@author: zakir
+"""
+import pdb
+
+
+pdb.set_trace()
+name=input("Enter Your Name:")
+age=input("Enter Your Age: ")
+age2=age+5
+print(age2)
+
+# l= list
+# c= continue
+# n= execute this line
+# q=quite
