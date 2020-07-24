afcd5d46434c138f377c5042f26feaac838952c7
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:31:20 +0600

    Add files via upload

---
+++ 09.string_formating.py
@@ -0,0 +1,7 @@
+name="Zakir"
+age=24
+print("Hello "+name+" your age is "+str(age)) #normal
+
+#String Formating
+print("Hello {} your age is {}".format(name,age)) # python 3 style
+print(f"Hello {name} your age is {age}")   #python 3.6 (best)
