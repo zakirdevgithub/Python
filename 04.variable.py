4d1e3573d8cfa0e7e87bd94947a6fc4ff906542e
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:28:16 +0600

    Add files via upload

---
+++ 04.variable.py
@@ -0,0 +1,17 @@
+var= "Zakir"
+print(var)
+var2="Jewel"
+var3=var+" "+var2 #string concatenation
+print(var3)
+
+
+name,age="Zakir","24"
+print(name)
+print(age)
+print(name,age) #string concatenation
+
+x=y=z=20
+print(x)
+print(y)
+print(z)
+print(x+y+z)
