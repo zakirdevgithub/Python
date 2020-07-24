6ddb2eced657e71cdb82ea4ed5da94836ef35391
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 19:32:16 +0600

    Add files via upload

---
+++ 10.string_indexing_slicing.py
@@ -0,0 +1,12 @@
+country="Bangladesh"
+print(country[0])
+print(country[1])
+print(country[2])
+print(country[3])
+
+print(country[:])
+print(country[3:])
+print(country[:5])
+print(country[:8])
+print(country[2:6])
+print(country[:-4])
